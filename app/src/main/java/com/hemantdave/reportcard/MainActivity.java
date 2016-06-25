e10827a38c5fb8986c08215e59c73e21b2641c9a
hkd27 <hdave@mail.bradley.edu>
2016-06-25 17:25:56 -0500

    Update MainActivity.java

--- app/src/main/java/com/hemantdave/reportcard/MainActivity.java
+++ app/src/main/java/com/hemantdave/reportcard/MainActivity.java
@@ -14,16 +14,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
        /* Here i have instantiaed the reportCard class*/
-        ReportCardPojo Student1=new ReportCardPojo("A","A","B","A","Donald","KAC-1283",2001);
-        ReportCardPojo Student2=new ReportCardPojo("A","B","B","C","Duck","KAC-1251",2001);
-        ReportCardPojo Student3=new ReportCardPojo("B","A","B","A","Tom","KBC-1211",2001);
-        ReportCardPojo Student4=new ReportCardPojo("C","B","B","C","Jerry","KAC-1101",2001);
+        reportCardPojo Student1=new reportCardPojo("A","A","B","A","Donald","KAC-1283",2001);
+        reportCardPojo Student2=new reportCardPojo("A","B","B","C","Duck","KAC-1251",2001);
+        reportCardPojo Student3=new reportCardPojo("B","A","B","A","Tom","KBC-1211",2001);
+        reportCardPojo Student4=new reportCardPojo("C","B","B","C","Jerry","KAC-1101",2001);
 
-        Log.i("Student 1 Report Card",Student1.GetReportCard());
-        Toast.makeText(MainActivity.this, "Student 1 Report Card"+Student1.GetReportCard(), Toast.LENGTH_SHORT).show();
+        Log.i("Student 1 Report Card",Student1.toString());
+        Toast.makeText(MainActivity.this, "Student 1 Report Card"+Student1.toString(), Toast.LENGTH_SHORT).show();
         TextView result=(TextView)findViewById(R.id.Result);
         assert result != null;
-        result.setText("Student 1 Report Card"+Student1.GetReportCard());
+        result.setText("Student 1 Report Card"+Student1.toString());
         Log.i("Student 2 only Name",Student2.getStudentName());
     }
 
